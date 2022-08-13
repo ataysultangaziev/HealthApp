4864068bc0779c388393c132a9ff80e761c344d3
asultangaziev <a.sultangaziev@bitlica.com>
2022-08-13 11:17:33 +0600

    Initial Commit

---
+++ HealthApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HealthApp
+//
+//  Created by Atay Sultangaziev on 13/8/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
