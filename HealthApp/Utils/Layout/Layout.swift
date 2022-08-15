a17a0c58aa763b58d4603ff81664c5962c39f7c9
asultangaziev <a.sultangaziev@bitlica.com>
2022-08-15 11:59:16 +0600

    Deleted storyboard and created utility Layout struct

---
+++ HealthApp/Utils/Layout/Layout.swift
@@ -0,0 +1,14 @@
+//
+//  Layout.swift
+//  HealthApp
+//
+//  Created by Atay Sultangaziev on 13/8/22.
+//
+
+import UIKit
+
+//Struct for default values
+public struct Layout {
+  public static let titleHeight: CGFloat = 50 as CGFloat
+  public static let calendarHeight: CGFloat = 80 as CGFloat
+}
